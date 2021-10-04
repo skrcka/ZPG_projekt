d121de341e5683acae8bcaea65e3f7137546f0af
skrcka <krcek.adam@gmail.com>
2021-10-04 10:16:27 +0200

    fixed leak

--- src/Objects/Object.h
+++ src/Objects/Object.h
@@ -4,4 +4,5 @@ class Object {
 public:
     virtual void applyShaders(const char *vertex_shader, const char *fragment_shader) = 0;
     virtual void draw() = 0;
+    virtual ~Object() = default;
 };
