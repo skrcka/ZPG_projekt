4329d653059547b6b303e7bbb59fbe8b62046911
skrcka <krcek.adam@gmail.com>
2021-10-04 22:54:13 +0200

    added VAO, VBO to Object

--- src/Objects/Object.h
+++ src/Objects/Object.h
@@ -5,4 +5,6 @@ public:
     virtual void applyShaders(const char *vertex_shader, const char *fragment_shader) = 0;
     virtual void draw() = 0;
     virtual ~Object() = default;
+private:
+    GLuint VAO, VBO;
 };
