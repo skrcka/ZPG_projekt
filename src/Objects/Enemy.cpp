3d9541f1cffa6d0bae58067d11eb1a1d797e34a9
skrcka <krcek.adam@gmail.com>
2021-12-07 11:51:52 +0100

    fixes2

--- src/Objects/Enemy.cpp
+++ src/Objects/Enemy.cpp
@@ -1,30 +1,37 @@
 #include "Enemy.h"
 
-Enemy::Enemy() : Object(), t(0.5f), delta(0.01f) {
+Enemy::Enemy() : Object(), t(0.5f), delta(0.01f), movementEnabled(false) {
     
 }
 Enemy::Enemy(Model *model, Shader *shader, Transform *transform)
-    : Object(model, shader, transform), t(0.5f), delta(0.01f)
+    : Object(model, shader, transform), t(0.5f), delta(0.01f), movementEnabled(false)
 {
 }
 Enemy::Enemy(Model *model, Shader *shader, Transform *transform, Texture *texture)
-    : Object(model, shader, transform, texture), t(0.5f), delta(0.01f)
+    : Object(model, shader, transform, texture), t(0.5f), delta(0.01f), movementEnabled(false)
 {
+}
+
+void Enemy::addMovement(glm::mat4x3 points){
+    this->points = points;
+    movementEnabled = true;
+}
+
+void Enemy::remMovement(){
+    movementEnabled = false;
 }
 
 void Enemy::move()
 {
+    if(!movementEnabled)
+        return;
     glm::mat4 A = glm::mat4(glm::vec4(-1.0, 3.0, -3.0, 1.0),
                             glm::vec4(3.0, -6.0, 3.0, 0),
                             glm::vec4(-3.0, 3.0, 0, 0),
                             glm::vec4(1, 0, 0, 0));
-    glm::mat4x3 B = glm::mat4x3(glm::vec3(-1, 0, 0),
-                                glm::vec3(0, 1, 0),
-                                glm::vec3(0, -1, 0),
-                                glm::vec3(1, 0, 0));
 
     glm::vec4 parameters = glm::vec4(t * t * t, t * t, t, 1.0f);
-    glm::vec3 p = parameters * A * glm::transpose(B);
+    glm::vec3 p = parameters * A * glm::transpose(points);
     this->transform->move(p.x, p.y, p.z);
 
     if (t >= 1.0f || t <= 0.0f)
--- src/Objects/Enemy.h
+++ src/Objects/Enemy.h
@@ -9,7 +9,11 @@ public:
     Enemy(Model *model, Shader *shader, Transform *transform);
     Enemy(Model *model, Shader *shader, Transform *transform, Texture *texture);
     void move();
+    void addMovement(glm::mat4x3 points);
+    void remMovement();
 
 private:
     float t, delta;
+    bool movementEnabled;
+    glm::mat4x3 points;
 };
