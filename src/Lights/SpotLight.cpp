3a8f09a591c30e18475095b075b4669ff086ee91
skrcka <krcek.adam@gmail.com>
2021-12-08 21:58:35 +0100

    fixed flashlight

--- src/Lights/SpotLight.cpp
+++ src/Lights/SpotLight.cpp
@@ -1,25 +1,32 @@
 #include "SpotLight.h"
 
-SpotLight::SpotLight() : PointLight(-1)
+SpotLight::SpotLight() : PointLight(-1), direction(glm::vec3(.0, .0, .0))
 {
-    direction = glm::vec3(.0, .0, .0);
+    this->cutOff = glm::cos(glm::radians(12.5f));
+    this->outerCutOff = glm::cos(glm::radians(15.f));
 }
 
 SpotLight::SpotLight(glm::vec3 position, glm::vec3 direction)
     : PointLight(-1, position), direction(direction)
 {
+    this->cutOff = glm::cos(glm::radians(12.5f));
+    this->outerCutOff = glm::cos(glm::radians(15.f));
 }
 
 SpotLight::SpotLight(glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, glm::vec3 position, glm::vec3 direction)
     : PointLight(ambient, diffuse, specular, -1, position), direction(direction)
 {
+    this->cutOff = glm::cos(glm::radians(12.5f));
+    this->outerCutOff = glm::cos(glm::radians(15.f));
 }
 
 SpotLight::~SpotLight() {}
 
 SpotLight::SpotLight(glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, glm::vec3 position, glm::vec3 direction, float cutOff, float outerCutOff)
-    : PointLight(ambient, diffuse, specular, -1, position), direction(direction), cutOff(cutOff), outerCutOff(outerCutOff)
+    : PointLight(ambient, diffuse, specular, -1, position), direction(direction)
 {
+    this->cutOff = glm::cos(glm::radians(cutOff));
+    this->outerCutOff = glm::cos(glm::radians(outerCutOff));
 }
 
 glm::vec3 SpotLight::getDirection()
@@ -27,10 +34,12 @@ glm::vec3 SpotLight::getDirection()
     return direction;
 }
 
-float SpotLight::getCutOff(){
+float SpotLight::getCutOff()
+{
     return cutOff;
 }
 
-float SpotLight::getOuterCutOff(){
+float SpotLight::getOuterCutOff()
+{
     return outerCutOff;
 }
